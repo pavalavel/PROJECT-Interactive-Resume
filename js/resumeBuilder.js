e3849ec2dcfdfc76cd4787f2d0e5b0cf46123d1f
Pavalavel <pavalavel.r1@gmail.com>
2015-07-20 00:43:04 +0530

    Added contact details to  header and footer

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,15 +2,57 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- var model={
+ var header={
  	"headerName":"Pavalavel Ramesh",
- 	"headerRole":"Web Developer"
- };
+ 	"headerRole":"Web Developer",
+ }
+ var contacts={
+ 	"type":"Skype",
+ 	"id":"Pavalavel Ramesh",
+ 	"mobile":9840684579,
+ 	"email":"pavalavel.r1@gmail.com",
+ 	"twitter":"@pavalavel_r",
+ 	"gitHub":"https://github.com/pavalavel",
+ 	"location":"Chennai"
+ 	}
+ 
 
  var displayHeader=function(){
- 	var name=HTMLheaderName.replace("%data%",model.headerName);
- 	var role=HTMLheaderRole.replace("%data%",model.headerRole);
+
+ 	var name=HTMLheaderName.replace("%data%",header.headerName);
+ 	var role=HTMLheaderRole.replace("%data%",header.headerRole);
  	var total=name+role;
+ 	
  	$('#header').append(total);
  };
  displayHeader();
+
+ var displayContact=function(){
+
+ 	var generic=HTMLcontactGeneric.replace("%contact%",contacts.type).replace("%data%",contacts.id);
+ 	var mobile=HTMLmobile.replace("%data%", contacts.mobile);
+ 	var email=HTMLemail.replace("%data%", contacts.email);
+ 	var github=HTMLgithub.replace("%data%", contacts.gitHub);
+
+ 	var totalTop=generic+mobile+email+github;
+	$('#topContacts').append(totalTop);
+
+	var twitter=HTMLtwitter.replace("%data%",contacts.twitter);
+	var location=HTMLlocation.replace("%data%",contacts.location);
+	var totalFooter=twitter+location;
+	$('#footerContacts').append(totalFooter);
+ };
+displayContact();
+
+
+
+
+
+
+
+
+
+
+
+
+
