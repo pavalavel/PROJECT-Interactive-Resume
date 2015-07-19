8b0573806601e63c11426c6f44072b5f602d3d02
Pavalavel <pavalavel.r1@gmail.com>
2015-07-19 23:55:35 +0530

    Header Added

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,16 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ var model={
+ 	"headerName":"Pavalavel Ramesh",
+ 	"headerRole":"Web Developer"
+ };
+
+ var displayHeader=function(){
+ 	var name=HTMLheaderName.replace("%data%",model.headerName);
+ 	var role=HTMLheaderRole.replace("%data%",model.headerRole);
+ 	var total=name+role;
+ 	$('#header').append(total);
+ };
+ displayHeader();
